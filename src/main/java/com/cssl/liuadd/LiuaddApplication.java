12e5312e7f1efdb4dcd4aa28a23c7fbaa573baab
liucheng <3334078524@qq.com>
2019-08-28 09:24:26 +0800

    Initial commit

---
+++ src/main/java/com/cssl/liuadd/LiuaddApplication.java
@@ -0,0 +1,13 @@
+package com.cssl.liuadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuaddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuaddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cssl/liuadd/LiuaddApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cssl.liuadd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiuaddApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
